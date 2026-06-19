04499b820dda4bf21d999d322c02e887875649ec
Karline Soetaert <karline.soetaert@nioz.nl>
2026-06-19 12:26:33 +0200

    XDCOPY - removed unrolled loops

--- src/inverse.f
+++ src/inverse.f
@@ -1,6 +1,6 @@
 C Karline: removed the write statement; where they also passed an integer value, 
 C this no long is the case. Each removed statement is preceded by:
-C KARLINE: REMOVED WRITE         
+C KARLINE: REMOVED roll from xDCOPY         
 
 
 C*********************************************************************
@@ -1427,7 +1427,7 @@ C
 C     COPIES A VECTOR, X, TO A VECTOR, Y.
 C     USES UNROLLED LOOPS FOR INCREMENTS EQUAL TO ONE.
 C     JACK DONGARRA, LINPACK, 3/11/78.
-C
+C karline: simplified the subroutine - removed unrolled loops
       DOUBLE PRECISION DX(*),DY(*)
       INTEGER I,INCX,INCY,IX,IY,M,MP1,N
 C
@@ -1452,23 +1452,27 @@ C        CODE FOR BOTH INCREMENTS EQUAL TO 1
 C
 C
 C        CLEAN-UP LOOP
-C
-   20 M = MOD(N,7)
-      IF( M .EQ. 0 ) GO TO 40
-      DO 30 I = 1,M
+C karline: this part added
+   20 DO I = 1, N
         DY(I) = DX(I)
-   30 CONTINUE
-      IF( N .LT. 7 ) RETURN
-   40 MP1 = M + 1
-      DO 50 I = MP1,N,7
-        DY(I) = DX(I)
-        DY(I + 1) = DX(I + 1)
-        DY(I + 2) = DX(I + 2)
-        DY(I + 3) = DX(I + 3)
-        DY(I + 4) = DX(I + 4)
-        DY(I + 5) = DX(I + 5)
-        DY(I + 6) = DX(I + 6)
-   50 CONTINUE
+      END DO
+! karline: this part removed      
+!   20 M = MOD(N,7)
+!      IF( M .EQ. 0 ) GO TO 40
+!      DO 30 I = 1,M
+!        DY(I) = DX(I)
+!   30 CONTINUE
+!      IF( N .LT. 7 ) RETURN
+!   40 MP1 = M + 1
+!      DO 50 I = MP1,N,7
+!        DY(I) = DX(I)
+!        DY(I + 1) = DX(I + 1)
+!        DY(I + 2) = DX(I + 2)
+!        DY(I + 3) = DX(I + 3)
+!        DY(I + 4) = DX(I + 4)
+!        DY(I + 5) = DX(I + 5)
+!        DY(I + 6) = DX(I + 6)
+!   50 CONTINUE
       RETURN
       END
 
