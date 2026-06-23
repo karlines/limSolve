526dfc70aa50bf2a335ed7aa59ce16884d3d264d
Karline Soetaert <karline.soetaert@nioz.nl>
2026-06-23 13:22:09 +0200

    kranke copy

--- src/inverse.f
+++ src/inverse.f
@@ -2270,11 +2270,11 @@ c     Move reduced problem data upward if KRANKE.LT.ME.
 c
       IF (KRANKE.LT.ME) THEN
          DO 200 J = 1,NP1
-            CALL xDCOPY (M-ME, W(ME+1,J), 1, W(KRANKE+1,J), 1)
+!            CALL xDCOPY (M-ME, W(ME+1,J), 1, W(KRANKE+1,J), 1)
 
-!           DO K = 1, M - ME
-!              W(KRANKE+K,J) = W(ME+K,J)
-!           END DO
+           DO K = 1, M - ME
+              W(KRANKE+K,J) = W(ME+K,J)
+           END DO
   200    CONTINUE
       ENDIF
 c
